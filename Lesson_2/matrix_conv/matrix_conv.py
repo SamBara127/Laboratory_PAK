a0b2dc5fcf7705d6ebee8508b9df30357f5ec258
Iv@n_NEEkot1K <92731967+SamBara127@users.noreply.github.com>
2023-09-18 12:25:20 +0700

    Correct 2 and add part of 3

--- Lesson_2/matrix_conv/matrix_conv.py
+++ Lesson_2/matrix_conv/matrix_conv.py
@@ -1,37 +1,52 @@
-lst = []
-lst2 = []
-sel = 0
-ls = []
-st = '1'
-with open('input.txt', 'r') as f:
-    while(st != ''):
-        st = f.readline()
-        if st != '\n':
-            st = st[:-1]
-            ls = st.split(' ')
-            if sel == 0:
-                lst.append(ls)
+
+def init_matrix(fn):
+    lst = []
+    lst2 = []
+    sel = 0
+    ls = []
+    st = '1'
+    with open(fn, 'r') as f:
+        while(st != ''):
+            st = f.readline()
+            if st != '\n':
+                st = st[:-1]
+                ls = st.split(' ')
+                if sel == 0:
+                    lst.append(ls)
+                else:
+                    lst2.append(ls)
             else:
-                lst2.append(ls)
-        else:
-            sel = 1
-lst2.pop()
-print(lst)
-print(lst2)
+                sel = 1
+    lst2.pop()
 
-if (len(lst[0]) < len(lst2[0]) or len(lst) < len(lst2)):
-    print('OPS!!!')
-    exit()
-print("OK")
+    if (len(lst[0]) < len(lst2[0]) or len(lst) < len(lst2)):
+        print('Данные матрицы нельзя перемножить!!!')
+        exit()
+    print("Выдана матрица №1 = ", end='')
+    print(lst)
+    print("Выдана матрица №2 = ", end='')
+    print(lst2)
 
-buf = 0
+    return lst, lst2
 
-with open('output.txt', 'w') as f:
-    for i in range(len(lst)-len(lst2)+1):
-        for j in range(len(lst[0])-len(lst2[0])+1):
-            for x in range(len(lst2)):
-                for y in range(len(lst2[0])): 
-                    buf += int(lst2[x][y]) * int(lst[x+i][y+j])
-            f.write(str(buf) + ' ')
-            buf = 0
-        f.write('\n')
+def matrix_conv(fn, lst, lst2):
+    buf = 0
+    with open(fn, 'w') as f:
+        for i in range(len(lst)-len(lst2)+1):
+            for j in range(len(lst[0])-len(lst2[0])+1):
+                for x in range(len(lst2)):
+                    for y in range(len(lst2[0])): 
+                        buf += int(lst2[x][y]) * int(lst[x+i][y+j])
+                f.write(str(buf) + ' ')
+                buf = 0
+            f.write('\n')
+    print('Файл с матрицей записан!')
+
+
+if __name__ == "__main__":
+
+    fin = 'input.txt'
+    fon = 'output.txt'
+
+    mat_1, mat_2 = init_matrix('input.txt')
+    matrix_conv('output.txt', mat_1, mat_2)
---
+++ Lesson_3/task_1.py
@@ -0,0 +1,59 @@
+class Item:
+    def __init__(self, count=3, max_count=16):
+        self._count = count
+        self._max_count = 16
+        
+    def update_count(self, val):
+        if val <= self._max_count:
+            self._count = val
+            return True
+        else:
+            return False
+        
+    # Свойство объекта. Не принимает параметров кроме self, вызывается без круглых скобок
+    # Определяется с помощью декоратора property
+    @property
+    def count(self):
+        return self._count
+    
+    
+    # Ещё один способ изменить атрибут класса
+    @count.setter
+    def count(self, val):
+        self._count = val
+        if val <= self._max_count:
+            self._counts = val
+        else:
+            pass
+    
+    @staticmethod
+    def static():
+        print('I am function')
+    
+    @classmethod
+    def my_name(cls):
+        return cls.__name__
+
+    def __add__(self, num):
+        """ Сложение с числом """
+        return self._count + num
+    
+    def __mul__(self, num):
+        """ Умножение на число """
+        return self._count * num
+    
+    def __lt__(self, num):
+        """ Сравнение меньше """
+        return self._count < num
+    
+    def __len__(self):
+        """ Получение длины объекта """
+        return self._count
+
+
+
+item = Item(2)
+print(item * 2)
+# print(item.count * 2)
+# ret = item.update_count(8)
+# print(ret, item.count)
