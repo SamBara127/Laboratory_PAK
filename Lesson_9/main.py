84de72ccffca76bb3960e7c2808c4e1594a8d64d
Iv@n_NEEkot1K <92731967+SamBara127@users.noreply.github.com>
2023-12-10 16:00:06 +0700

    MODUFY_9

--- Lesson_9/main.py
+++ Lesson_9/main.py
@@ -5,6 +5,9 @@ from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import train_test_split
 from xgboost import XGBClassifier
 from sklearn.linear_model import LogisticRegression
+from sklearn.base import BaseEstimator
+from sklearn.utils import shuffle
+import numpy as np
 
 
 def get_train_and_test(df):
@@ -51,6 +54,45 @@ def most_important(y_test, x_test, y_train, x_train, amount_of_properties):
     return res, acc
 
 
+
+class MyRandomForest(BaseEstimator):
+    # для построения слуаяного леса будем использовать так называемых "отдельных оценщиков" 
+    # из класса BaseEstimator. Каждому мы отдадим 
+    def __init__(self, n_estimators=50, max_depth=None, random_state=None):
+        self.n_estimators = n_estimators
+        self.max_depth = max_depth
+        self.random_state = random_state
+        self.estimators = []
+
+    def fit(self, X, y):
+        np.random.seed(self.random_state)
+        self.estimators = []
+
+        X_bootstrap = np.array_split(X, self.n_estimators)
+        y_bootstrap = np.array_split(y, self.n_estimators)
+
+        for i in range(self.n_estimators):
+            # Bootstrap sampling
+            # indices = np.random.choice(X.shape[0], X.shape[0], replace=True)
+            # X_bootstrap = X[indices]
+            # y_bootstrap = y[indices]
+
+            # Create and train a decision tree
+            tree = DecisionTreeClassifier(max_depth=self.max_depth, random_state=self.random_state)
+            tree.fit(X_bootstrap[i], y_bootstrap[i])
+
+            # Save the trained tree
+            self.estimators.append(tree)
+
+    def predict(self, X):
+        # Aggregate predictions from all trees
+        predictions = np.array([tree.predict(X) for tree in self.estimators])
+        # Use majority voting to get the final prediction
+        final_predictions = np.apply_along_axis(lambda x: np.bincount(x).argmax(), axis=0, arr=predictions)
+        return final_predictions
+    
+
+
 if __name__ == "__main__":
     tab = pd.read_csv('titanic_prepared.csv', index_col=0, delimiter=',')
     # делаем сплит датасета на тестовую и обучающую выборку
@@ -60,3 +102,33 @@ if __name__ == "__main__":
     _, acc2 = xgboost(y_test, x_test, y_train, x_train)
     _, acc3 = logistic_regression(y_test, x_test, y_train, x_train)
     _, acc4 = most_important(y_test, x_test, y_train, x_train, 2)
+
+    print(acc1)
+    print(acc2)
+    print(acc3)
+    print(acc4)
+
+    # Создание и обучение MyRandomForest
+    my_rf = MyRandomForest()
+    # for i in range(10):
+    
+    # print(x_train)        
+    # indices = np.random.choice(x_train.shape[0], x_train.shape[0], replace=True)
+    # print(x_train.iloc[indices])
+    # print(shuffle(x_train))
+    # print('SPLITTER')
+    # num_splits = 100
+    # x_train_splits = np.array_split(x_train, num_splits)
+    # y_train_splits = np.array_split(y_train, num_splits)
+    # print(x_train_splits[0])
+    # print(y_train_splits[0])
+
+    my_rf.fit(x_train, y_train)
+
+    # Прогнозирование
+    y_pred = my_rf.predict(x_test)
+
+    # Оценка точности
+    accuracy = sklearn.metrics.accuracy_score(y_test, y_pred)
+    # print(f'Accuracy: {accuracy:.2f}')
+    print(f'Accuracy: {accuracy}')
